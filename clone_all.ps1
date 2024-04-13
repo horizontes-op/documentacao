c0a0eb715cadc3377e4e778c7aa7a0f1c29f6fb5
esdrasgc <esdrasgcdev@gmail.com>
2024-04-12 22:20:52 -0300

    script para clonar repos

---
+++ clone_all.ps1
@@ -0,0 +1,14 @@
+cd ..
+gh repo clone horizontes-op/instituicao
+gh repo clone horizontes-op/account
+gh repo clone horizontes-op/account-resource
+gh repo clone horizontes-op/aluno
+gh repo clone horizontes-op/auth
+gh repo clone horizontes-op/discovery
+gh repo clone horizontes-op/gateway
+gh repo clone horizontes-op/instituicao-resource
+gh repo clone horizontes-op/recomendacao
+gh repo clone horizontes-op/aluno-resource
+gh repo clone horizontes-op/auth-resource
+gh repo clone horizontes-op/recomedacao-resource
+gh repo clone horizontes-op/docker-compose
